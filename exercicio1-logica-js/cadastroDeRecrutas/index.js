d007476be37a647b51aad2659c58944ca2107c18
Daniel Paes <daniel_3xd@hotmail.com>
2022-12-20 22:48:48 -0300

    exercicio1 pronto

---
+++ exercicio1-logica-js/cadastroDeRecrutas/index.js
@@ -0,0 +1,6 @@
+let nome = prompt('Digite seu nome: ')
+let sobrenome = prompt('Digite seu sobrenome: ')
+let campoDeEstudo = prompt('Digite seu campo de estudo: ')
+let anoNascimento = prompt('Digite seu ano de nascimento: ')
+
+alert('Informações cadastradas:\n' + 'Nome: ' + nome + '\nSobrenome: ' + sobrenome + '\nCampo de Estudo: ' + campoDeEstudo + '\nAno de Nascimento: ' + anoNascimento)
---
+++ exercicio1-logica-js/calculadora4Operacoes/index.js
@@ -0,0 +1,38 @@
+let num = parseFloat(prompt('Digite o primeiro número: '))
+let num2 = parseFloat(prompt('Digite o segundo número: '))
+
+let opcao = parseFloat(prompt('Digite qual operação deseja fazer, sendo: \n1-Soma\n2-Subtração\n3-Multiplicação\n4-Divisão'))
+
+switch (opcao) {
+    case 1:
+        somar();
+        break;
+    case 2:
+        subtrair();
+        break;
+    case 3:
+        multiplicar();
+        break;
+    case 4:
+        dividir();
+        break;
+    default:
+        alert('Digite um dos valores solicitados!')
+        break;
+}
+
+function somar(){
+    alert(num + num2)
+}
+
+function subtrair(){
+    alert(num - num2)
+}
+
+function multiplicar(){
+    alert(num * num2)
+}
+
+function dividir(){
+    alert(num / num2)
+}
