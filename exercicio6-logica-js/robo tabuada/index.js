7f49aeeb400212939612e95864e21365d0b7ae7f
Daniel Paes <daniel_3xd@hotmail.com>
2022-12-29 00:57:04 -0300

    robo tabuada pronto

--- exercicio6-logica-js/robo tabuada/index.js
+++ exercicio6-logica-js/robo tabuada/index.js
@@ -1,4 +1,17 @@
+let num = prompt('Digite um número: ');
 
+let resultado = '';
+
+let resultados = [];
+
+for (let i = 1; i <= 20; i++) {
+
+    resultado = num * i;
+
+    resultados[i] = resultado;
+}
+
+alert('Resultado da multiplicação de ' + num + ' entre 1 à 20:\n\n' + resultados);
 
 /* Escreva um programa em javascript que seja capaz de calcular
 a tabuada de um determinado número (de 1 a 20). O programa deve 
