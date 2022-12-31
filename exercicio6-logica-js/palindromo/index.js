354ffa4067121256a3d4a5aae60985af27277f28
Daniel Paes <daniel_3xd@hotmail.com>
2022-12-31 17:04:17 -0300

    exercicio 6 pronto

--- exercicio6-logica-js/palindromo/index.js
+++ exercicio6-logica-js/palindromo/index.js
@@ -1,4 +1,17 @@
 
+const palavra = prompt('Digite uma palavra: ');
+
+let palavraInvertida = '';
+
+for (let i = palavra.length - 1; i >= 0; i--) {
+    palavraInvertida += palavra[i];
+}
+
+if(palavra == palavraInvertida){
+    alert(palavra + " é um Palindromo!")
+} else {
+    alert(palavra + " não é um Palindromo!")
+}
 
 /* Escreva um programa em javascript que seja capaz de identificar
 se uma palavra é um palíndromo. 
